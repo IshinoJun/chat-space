a982ea2e307022c920adf2b8577aaf69d23a8135
IshinoJun <junishino@junnoMacBook-puro.local>
2019-01-13 16:51:24 +0900

    HTMLを生成するメソッドを作成

--- app/assets/javascripts/channels/message.js
+++ app/assets/javascripts/channels/message.js
@@ -1,4 +1,22 @@
 $(function(){
+  function buildHTML(message){
+    if(message.image){
+      var imagePresent = `<img class="lower-message__image" src=${message.image}>`
+    }
+    var html = `<div class="chat-main__message">
+                  <div class="chat-main__message-name">
+                    ${message.name}
+                  </div>
+                  <div class="chat-main__message-time">
+                    ${message.created_at}
+                  </div>
+                  <div class="chat-main__message-body">
+                    ${message.content}
+                    ${imagePresent}
+                  </div>
+                </div>`
+    return html
+  }
   $('#new_message').on('submit',function(e){
     e.preventDefault();
     var formData = new FormData(this);
