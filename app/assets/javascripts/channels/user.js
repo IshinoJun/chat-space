40d72448cc12c3d0e53e511e10e5bb50d5c518db
IshinoJun <junishino@junnoMacBook-puro.local>
2019-01-14 17:17:18 +0900

    HTMLの修正

--- app/assets/javascripts/channels/user.js
+++ app/assets/javascripts/channels/user.js
@@ -1,7 +1,9 @@
 $(function(){
+  var search_result = $("#user-search-result");
+
   function appendUser(user) {
     var html = `<div class="chat-group-user clearfix">
-                  <p class="chat-group-user__name">${user.mame}</p>
+                  <p class="chat-group-user__name">${user.name}</p>
                   <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</a>
                 </div>`
     search_result.append(html);
@@ -10,31 +12,32 @@ $(function(){
 
   function appendNoUser(message) {
     var html = `<div class="chat-group-user clearfix">
-                  <p class="chat-group-user__name">${message}</p>`
+                  <p class="chat-group-user__name">${message}</p>
+                </div>`
     search_result.append(html);
-
   }
 
   $("#user-search-field").on("keyup",function(){
-    var input = $("#user-search-field").val();
+    var input = $(this).val();
 
     $.ajax({
       type: 'GET',
-      url: '/users/index',
+      url: '/users',
       data: { keyword: input },
       dataType: 'json'
     })
 
     .done(function(users){
-      $(".user-search-result").empty();
-      if (users.length !==0) {
-        users.forEach(function(user){
-          appendUser(user);
-        });
-      }
-      else {
-        appendNoUser("一致するユーザーは見つかりません");
-      }
+      $("#user-search-result").empty();
+      if (users.length !== 0) {
+         users.forEach(function(user){
+           appendUser(user);
+         });
+       }
+       else {
+         appendNoUser("一致するユーザーは見つかりませんでした");
+       }
+      })
     })
   });
 });
