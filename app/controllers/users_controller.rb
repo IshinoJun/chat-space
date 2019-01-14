c73ce499412ae35f6dbc13cd098d41aa9d9cadc6
IshinoJun <junishino@junnoMacBook-puro.local>
2019-01-14 15:02:30 +0900

    jbuilderでhtmlとjsonを分岐

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,11 @@
 class UsersController < ApplicationController
 
   def index
+    @users = User.where(activated: true).paginate(page: params[:page]).search(params[:search])
+    respond_to do |format|
+      format.html
+      format.json
+    end
   end
 
   def edit
