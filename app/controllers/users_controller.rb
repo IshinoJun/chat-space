1056239209bdbf93a417a7bfc38aed2f196733c9
IshinoJun <junishino@junnoMacBook-puro.local>
2019-01-14 17:18:26 +0900

    コントローラの検索修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
 
   def index
-    @users = User.where(activated: true).paginate(page: params[:page]).search(params[:search])
+    @users = User.where('name LIKE(?)', "%#{params[:keyword]}%").where.not(id: current_user.id)
     respond_to do |format|
       format.html
       format.json
