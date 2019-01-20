fb397be6f8157745a27285824d5313d23c6f5854
IshinoJun <junishino@junnoMacBook-puro.local>
2019-01-20 18:19:39 +0900

    修正

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,7 +9,7 @@ set :rbenv_type, :user
 set :rbenv_ruby, '2.3.1'
 
 set :ssh_options, auth_methods: ['publickey'],
-                  keys: ['~/.ssh/TEHC:EXPERT.pem']
+                  keys: ['~/.ssh/TEHCEXPERT.pem']
 
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
